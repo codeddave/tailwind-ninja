0f86bf90cbec04d82d7c10b7da34c6913a215a5f
Dvaid Adeleye <drizzzydave@gmail.com>
2020-11-16 08:30:06 -0800

    added custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363', 
+
+        secondary: {
+          100: '#E2E2D5', 
+          200: '#888883', 
+        }
+      }, 
+      fontFamily: {
+        david: ['Roboto']
+      }
+    },
   },
   variants: {},
   plugins: [],
